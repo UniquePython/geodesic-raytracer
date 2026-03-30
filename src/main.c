ce21d68f2a06666a53a989eff950fe11bf64d3c0
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-30 14:13:57 +0530

    write runge kutta 4 function

--- src/main.c
+++ src/main.c
@@ -26,3 +26,48 @@ RayState derivatives(RayState currState, double angularMomentum)
 
     return derivative;
 }
+
+RayState rk4_step(RayState currState, double angularMomentum, double dlambda)
+{
+    RayState k1 = derivatives(currState, angularMomentum);
+
+    // --- K2 ------------>
+
+    RayState k2Input = {
+        .radius = currState.radius + (k1.radius * dlambda / 2),
+        .dRadius = currState.dRadius + (k1.dRadius * dlambda / 2),
+        .angle = currState.angle + (k1.angle * dlambda / 2),
+    };
+
+    RayState k2 = derivatives(k2Input, angularMomentum);
+
+    // --- K3 ------------>
+
+    RayState k3Input = {
+        .radius = currState.radius + (k2.radius * dlambda / 2),
+        .dRadius = currState.dRadius + (k2.dRadius * dlambda / 2),
+        .angle = currState.angle + (k2.angle * dlambda / 2),
+    };
+
+    RayState k3 = derivatives(k3Input, angularMomentum);
+
+    // --- K4 ------------>
+
+    RayState k4Input = {
+        .radius = currState.radius + (k3.radius * dlambda),
+        .dRadius = currState.dRadius + (k3.dRadius * dlambda),
+        .angle = currState.angle + (k3.angle * dlambda),
+    };
+
+    RayState k4 = derivatives(k4Input, angularMomentum);
+
+    // --- Final Blend ------------>
+
+    RayState result = {
+        .radius = currState.radius + (dlambda / 6) * (k1.radius + 2 * k2.radius + 2 * k3.radius + k4.radius),
+        .angle = currState.angle + (dlambda / 6) * (k1.angle + 2 * k2.angle + 2 * k3.angle + k4.angle),
+        .dRadius = currState.dRadius + (dlambda / 6) * (k1.dRadius + 2 * k2.dRadius + 2 * k3.dRadius + k4.dRadius),
+    };
+
+    return result;
+}
