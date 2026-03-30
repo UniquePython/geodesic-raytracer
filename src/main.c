e49202ed74a8cbc4ca8293e9651b33b4478bf768
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-30 13:53:27 +0530

    define ray state

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("geodesic-raytracer\n");
-    return 0;
-}
+typedef struct
+{
+    double radius;  // Radius of ray in polar coordinates
+    double angle;   // Angle of ray in polar coordinates
+    double dRadius; // Rate of change of radius
+} RayState;         // Rate of change of angle omitted - derived as `L/r^2`
