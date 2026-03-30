ffe9bf2a824e079ce4a7dd0fa4bf15f40339a491
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-30 14:03:25 +0530

    write derivative function

--- src/main.c
+++ src/main.c
@@ -3,7 +3,26 @@ typedef struct
     double radius;  // Radius of ray in polar coordinates
     double angle;   // Angle of ray in polar coordinates
     double dRadius; // Rate of change of radius
-} RayState;         // Rate of change of angle omitted - derived as `L/r^2`
+} RayState;         // Rate of change of angle omitted - derived as `L/r^2` where L is angular momentum
 
 #define RS 1.0     // Schwarzschild radius, normalized to 1
 #define R_CAM 20.0 // camera distance from black hole
+
+RayState derivatives(RayState currState, double angularMomentum)
+{
+    RayState derivative;
+
+    derivative.radius = currState.dRadius;
+
+    double radiusSquared = currState.radius * currState.radius;
+    double angularMomentumSquared = angularMomentum * angularMomentum;
+
+    derivative.angle = angularMomentum / radiusSquared;
+
+    double outwardCentrifugalPush = angularMomentumSquared / (radiusSquared * currState.radius);
+    double inwardPull = 3 * angularMomentumSquared * RS / (2 * radiusSquared * radiusSquared);
+
+    derivative.dRadius = outwardCentrifugalPush - inwardPull;
+
+    return derivative;
+}
