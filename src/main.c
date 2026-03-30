a75234cdb11ffe0bd2cdef2e3ee93e67490d6fee
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-30 13:54:48 +0530

    define constants

--- src/main.c
+++ src/main.c
@@ -4,3 +4,6 @@ typedef struct
     double angle;   // Angle of ray in polar coordinates
     double dRadius; // Rate of change of radius
 } RayState;         // Rate of change of angle omitted - derived as `L/r^2`
+
+#define RS 1.0     // Schwarzschild radius, normalized to 1
+#define R_CAM 20.0 // camera distance from black hole
